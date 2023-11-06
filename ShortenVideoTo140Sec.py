5697c9c8a049a454e89c0fbeb9c47bee60d9d6a9
O Jongbo <15217557+PiesP@users.noreply.github.com>
2023-11-07 00:16:43 +0900

    [Update] 비디오 처리 취소 기능 추가 및 사용자 피드백 개선
    사용자가 비디오 처리를 취소할 수 있도록 '취소' 버튼이 추가되었습니다. 처리가 취소될 경우, 이미 생성된 출력 파일을 자동으로 삭제하여 시스템을 깨끗하게 유지합니다. 또한, 사용자 인터페이스에 현재 진행 상태를 실시간으로 보여주는 상태 바를 추가하여 사용자가 처리 과정을 더 잘 이해할 수 있도록 하였습니다. ProgressBarLogger가 상태 업데이트를 처리하도록 확장되었으며, 사용자 경험을 향상시키기 위해 애플리케이션 창의 크기를 조정했습니다.

--- ShortenVideoTo140Sec.py
+++ ShortenVideoTo140Sec.py
@@ -5,61 +5,79 @@ import threading
 from moviepy.editor import VideoFileClip, vfx, ImageClip, concatenate_videoclips
 from proglog import ProgressBarLogger
 
-# Custom Proglog logger
 class TkProgressBarLogger(ProgressBarLogger):
-    def __init__(self, progress_var, max_value):
+    def __init__(self, progress_var, max_value, status_var, cancel_event):
+        super().__init__()
         self.progress_var = progress_var
         self.max_value = max_value
-        super().__init__()
+        self.status_var = status_var
+        self.cancel_event = cancel_event
 
     def callback(self, **changes):
+        if self.cancel_event.is_set():
+            raise Exception("Video processing was cancelled by user.")
         if 'index' in changes:
-            self.progress_var.set(changes['index'] / self.max_value * 100)
+            progress_percentage = changes['index'] / self.max_value * 100
+            self.progress_var.set(progress_percentage)
+            self.status_var.set(f'Processing... {progress_percentage:.2f}%')
 
     def bars_callback(self, bar, attr, value, old_value=None):
         if attr == 'index':
             percentage = (value / self.bars[bar]['total']) * 100
             self.progress_var.set(percentage)
+            self.status_var.set(f'{bar.capitalize()} processing: {value}/{self.bars[bar]["total"]} ({percentage:.2f}%)')
 
-# File selection function
 def select_file(title, filetypes):
     return filedialog.askopenfilename(title=title, filetypes=filetypes)
 
-# File extension validation function
 def validate_file_extension(file_path, valid_extensions):
     _, ext = os.path.splitext(file_path)
     return ext.lower() in valid_extensions
 
-# Main application class
 class VideoProcessorApp:
     def __init__(self, root):
         self.root = root
-        self.root.title('Video Processing')
+        self.root.title('Video Processing App')
+        self.root.geometry('300x100')  # Make sure the height accommodates the Cancel button.
+
+        self.status_var = tk.StringVar()
+        self.status_label = tk.Label(root, textvariable=self.status_var)
+        self.status_label.pack(fill=tk.X, expand=True, padx=20, pady=5)
+
         self.progress_var = tk.DoubleVar()
         self.progress_bar = ttk.Progressbar(root, variable=self.progress_var, maximum=100)
-        self.progress_bar.pack(fill=tk.X, expand=True, padx=20, pady=20)
+        self.progress_bar.pack(fill=tk.X, expand=True, padx=20, pady=5)
+
+        self.cancel_button = ttk.Button(root, text="Cancel", command=self.cancel_processing)
+        self.cancel_button.pack(fill=tk.X, expand=True, padx=20, pady=5)
+
+        self.cancel_event = threading.Event()
+
         self.show_dialogs()
 
     def show_dialogs(self):
         try:
             video_path = select_file('Select a video file', [('Video files', '*.mp4 *.mkv *.avi *.mov *.flv')])
-            if not video_path or not validate_file_extension(video_path, ['.mp4', '.mkv', '.avi', '.mov', '.flv']):
+            if video_path and not validate_file_extension(video_path, ['.mp4', '.mkv', '.avi', '.mov', '.flv']):
                 raise ValueError('Unsupported video file format.')
+            elif not video_path:
+                return
 
             image_path = select_file('Select an image file to insert (skip if not needed)', [('Image files', '*.jpg *.jpeg *.png *.bmp *.gif')])
             if image_path and not validate_file_extension(image_path, ['.jpg', '.jpeg', '.png', '.bmp', '.gif']):
                 raise ValueError('Unsupported image file format.')
+            elif not image_path:
+                image_path = None
 
             self.process_video(video_path, image_path)
         except ValueError as e:
             messagebox.showerror('Error', str(e))
 
     def process_video(self, video_path, image_path):
-        # Start the video processing in a separate thread
         threading.Thread(target=self._process_video_thread, args=(video_path, image_path)).start()
 
     def _process_video_thread(self, video_path, image_path):
-        # This method will be executed in a separate thread
+        adjusted_filename = os.path.splitext(video_path)[0] + '_adjusted.mp4'
         try:
             clip = VideoFileClip(video_path)
             if image_path:
@@ -70,21 +88,45 @@ class VideoProcessorApp:
             if video_duration > 139.98:
                 speedup_factor = video_duration / 139.98
                 adjusted_clip = clip.fx(vfx.speedx, speedup_factor)
-                logger = TkProgressBarLogger(self.progress_var, 100)
-                adjusted_filename = os.path.splitext(video_path)[0] + '_adjusted.mp4'
+                logger = TkProgressBarLogger(self.progress_var, 100, self.status_var, self.cancel_event)
+
+                if self.cancel_event.is_set():
+                    raise Exception("Video processing was cancelled.")
+
                 adjusted_clip.write_videofile(adjusted_filename, logger=logger)
+
+                if self.cancel_event.is_set():
+                    raise Exception("Video processing was cancelled.")
+
                 clip.close()
                 if image_path:
                     image_clip.close()
-                self.root.after(0, lambda: messagebox.showinfo('완료', f'조절된 동영상이 {adjusted_filename}로 저장되었습니다.'))
-                self.root.after(0, self.root.destroy)  # Close the main window and exit the program
-            else:
-                self.root.after(0, lambda: messagebox.showinfo('정보', '동영상의 길이가 이미 139초 이하입니다.'))
-                self.root.after(0, self.root.destroy)  # Close the main window and exit the program
-        except Exception as e:
-            self.root.after(0, lambda: messagebox.showerror('Error', str(e)))
 
-# Run the application
+                self.root.after(0, lambda: messagebox.showinfo('Complete', f'Adjusted video saved as {adjusted_filename}.'))
+                self.root.after(0, self.root.destroy)
+            else:
+                self.root.after(0, lambda: messagebox.showinfo('Info', 'Video duration is already below 140 seconds.'))
+                self.root.after(0, self.root.destroy)
+        except Exception as e:
+            error_message = str(e)
+            self.root.after(0, lambda: messagebox.showerror('Error', error_message))
+            if os.path.exists(adjusted_filename):
+                try:
+                    os.remove(adjusted_filename)
+                    self.root.after(0, lambda: messagebox.showinfo('Info', 'Cancelled and cleaned up incomplete video file.'))
+                except Exception as cleanup_error:
+                    self.root.after(0, lambda: messagebox.showerror('Error', f'Failed to delete incomplete video file: {cleanup_error}'))
+            self.root.after(0, self.root.destroy)
+        finally:
+            clip.close()
+            if image_path and 'image_clip' in locals():
+                image_clip.close()
+
+    def cancel_processing(self):
+        self.cancel_event.set()
+        self.status_var.set('Processing cancelled.')
+        self.cancel_button.config(state=tk.DISABLED)
+
 if __name__ == '__main__':
     root = tk.Tk()
     app = VideoProcessorApp(root)
