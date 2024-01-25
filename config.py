3db6a4ac6424803a8d600e28ce6d56dd8ed8d0af
Jongbo Oh <15217557+PiesP@users.noreply.github.com>
2024-01-26 07:37:04 +0900

    We deleted the unnecessary files.

--- config.py
+++
@@ -1,4 +0,0 @@
-VIDEO_FILE_TYPES = [("Video files", "*.mp4 *.avi *.mov *.mkv *.flv *.wmv *.mpeg *.mpg *.webm *.ogg")]
-IMAGE_FILE_TYPES = [("Image files", "*.jpg *.jpeg *.png *.bmp *.gif *.tif *.tiff *.webp")]
-ENCODER = None 
-CREATE_NO_WINDOW = 0x08000000
